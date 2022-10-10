d2011968bd9befcb72a37db5aa406307ed78d97f
Rakesh Baruah <rbaruah@gmail.com>
2022-10-10 10:56:15 -0500

    add getFormattedDate util function

--- components/ExpensesOutput/ExpenseItem.tsx
+++ components/ExpensesOutput/ExpenseItem.tsx
@@ -6,6 +6,7 @@ import {
   ShadowPropTypesIOS,
 } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
+import { getFormattedDate } from "../../util/date";
 
 export default function ExpenseItem({ description, amount, date }) {
   return (
@@ -15,7 +16,7 @@ export default function ExpenseItem({ description, amount, date }) {
           <Text style={[styles.textBase, styles.description]}>
             {description}
           </Text>
-          <Text style={styles.textBase}>{date.toString()}</Text>
+          <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
         </View>
         <View style={styles.amountContainer}>
           <Text style={styles.amount}>{amount}</Text>
