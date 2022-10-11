4f5e520e0b44c9fb343355473e5028a379e2c134
Rakesh Baruah <rbaruah@gmail.com>
2022-10-11 15:24:29 -0500

    add trash icon to manage expense screen

--- screens/ManageExpense.tsx
+++ screens/ManageExpense.tsx
@@ -1,5 +1,7 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import IconButton from "../components/ui/IconButton";
+import { GlobalStyles } from "../constants/styles";
 
 export default function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -12,5 +14,36 @@ export default function ManageExpense({ route, navigation }) {
     });
   }, [isEditing, navigation]);
 
-  return <Text>Manage Expense Screen</Text>;
+  function deleteExpenseHandler() {}
+
+  return (
+    <View style={styles.container}>
+      
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
