c3580ca9c70e812927fc09cab8879672e48355e1
Rakesh Baruah <rbaruah@gmail.com>
2022-10-12 18:07:40 -0500

    add input button styles

--- components/ManageExpense/Input.tsx
+++ components/ManageExpense/Input.tsx
@@ -1,4 +1,5 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 export default function Input({ label, textInputConfig }) {
   return (
@@ -8,3 +9,21 @@ export default function Input({ label, textInputConfig }) {
     </View>
   );
 }
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 16,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary700,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+});
